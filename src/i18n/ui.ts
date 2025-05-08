214bc2ef43c660f3e6907d7dc36726c9167e68e1
Iris Salcedo Garcia <3985124+isalga@users.noreply.github.com>
2025-05-08 18:13:02 +0200

    Add twinning and friendship page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -15,12 +15,13 @@ export const ui = {
     welcome: 'Welcome to the ',
     donate: 'Donate Now',
     learn_more: 'Learn more',
-    description: 'Empowering Jabalia\'s future through education, health, and community support—building a rooted, resilient, and liberated community for children, families, and generations to come.',
+    description:
+      "Empowering Jabalia's future through education, health, and community support—building a rooted, resilient, and liberated community for children, families, and generations to come.",
     features_title: 'Activities and Programs',
-    features_subtitle: 'The Palestine Blossoms will play a vital role in supporting the community in the Jabalia refugee camp and the other refugees\' camps in the Gaza Strip.',
+    features_subtitle:
+      "The Palestine Blossoms will play a vital role in supporting the community in the Jabalia refugee camp and the other refugees' camps in the Gaza Strip.",
     headline_title: 'Find out how to support',
     headline_subtitle: 'There are many ways to support',
-
 
     'programs.kindergarten.title': 'Kindergarten',
     'programs.kindergarten.description': 'Providing quality early childhood education in a safe environment.',
@@ -35,7 +36,23 @@ export const ui = {
     'programs.fourth.description': 'tbd',
 
     tagline: 'Building a better future',
-    classes: 'classes'
+    classes: 'classes',
+    'twinningandfrienship.firstHero.title': 'Localization of Palestine and Global Solidarity',
+    'twinningandfrienship.firstHero.subtitle': `The Jabalia Institute for Community Development and Education aims to localize the camp within the Gaza Strip and
+      beyond, creating a more accurate and empathetic understanding of the camp and its people for those observing from
+      afar. By fostering mutual aid and support, we ensure dignified engagement in every aspect of the organization’s
+      programs.`,
+    'twinningandfrienship.secondHero.subtitle': `By focusing on these areas, the Twinning and Friendship Program will build a strong foundation for global
+      solidarity, fostering a supportive and nurturing environment for the entire communities. This approach will help
+      to humanize the refugee experience and create meaningful connections that emphasize the political nature of the
+      Palestinian struggle for justice and self-determination.`,
+    'twinningandfrienship.thirdHero.subtitle': `By focusing on these areas, the Jabalia Institute for Community Development and Education could significantly
+      improve the quality of life for children, mothers, and families in the Jabalia refugee camp, fostering a
+      supportive and nurturing environment for a targeted and selected group whom we would have a long sustainable
+      relation to while we are enriching and extend our impact to the entire community.`,
+    'twinningandfrienship.feature1.title': 'Objectives',
+    'twinningandfrienship.feature2.title': 'Goals',
+    'twinningandfrienship.feature3.title': 'Program Activities',
   },
   ar: {
     'nav.home': 'فلسطين',
@@ -45,9 +62,11 @@ export const ui = {
     welcome: 'مرحبًا بكم في مركز جباليا',
     donate: 'تبرع الآن',
     learn_more: 'يتعلم أكثر',
-    description: 'تمكين مستقبل جباليا من خلال التعليم، الصحة، ودعم المجتمع—بناء مجتمع متجذر، مرن، ومحرر للأطفال، العائلات، والأجيال القادمة.',
+    description:
+      'تمكين مستقبل جباليا من خلال التعليم، الصحة، ودعم المجتمع—بناء مجتمع متجذر، مرن، ومحرر للأطفال، العائلات، والأجيال القادمة.',
     features_title: 'الأنشطة والبرامج',
-    features_subtitle: 'وستلعب مبادرة "أزهار فلسطين" دوراً حيوياً في دعم المجتمع في مخيم جباليا للاجئين ومخيمات اللاجئين الأخرى في قطاع غزة.',
+    features_subtitle:
+      'وستلعب مبادرة "أزهار فلسطين" دوراً حيوياً في دعم المجتمع في مخيم جباليا للاجئين ومخيمات اللاجئين الأخرى في قطاع غزة.',
     headline_title: 'أنشطة وبرامج المنظمة',
     headline_subtitle: 'اكتشف كيف يمكننا تقديم الدعم',
 
@@ -64,8 +83,24 @@ export const ui = {
     'programs.fourth.description': 'سيُعلن لاحقاً',
 
     tagline: 'بناء مستقبل أفضل',
-    classes: 'classes'
-  }
+    classes: 'classes',
+    'twinningandfrienship.firstHero.title': 'توطين فلسطين والتضامن العالمي',
+    'twinningandfrienship.firstHero.subtitle': `يهدف معهد جباليا للتنمية والتثقيف المجتمعي إلى توطين المخيم داخل قطاع غزة و
+      خارجه، مما يخلق فهماً أكثر دقة وتعاطفاً للمخيم وسكانه لأولئك الذين يراقبون من
+      من بعيد. ومن خلال تعزيز المساعدة والدعم المتبادل، نضمن المشاركة الكريمة في كل جانب من جوانب
+      برامج المنظمة.`,
+    'twinningandfrienship.secondHero.subtitle': `من خلال التركيز على هذه المجالات، سيبني برنامج التوأمة والصداقة أساسًا قويًا للتضامن العالمي
+      التضامن العالمي، وتعزيز بيئة داعمة ورعاية للمجتمعات بأكملها. سيساعد هذا النهج على
+      على إضفاء الطابع الإنساني على تجربة اللاجئين وخلق روابط ذات مغزى تؤكد على الطبيعة السياسية
+      النضال الفلسطيني من أجل العدالة وتقرير المصير.`,
+    'twinningandfrienship.thirdHero.subtitle': `ومن خلال التركيز على هذه المجالات، يمكن لمعهد جباليا للتنمية المجتمعية والتعليم أن
+      تحسين نوعية حياة الأطفال والأمهات والعائلات في مخيم جباليا للاجئين، وتعزيز
+      بيئة داعمة وحاضنة لمجموعة مستهدفة ومختارة ستكون لنا علاقة مستدامة طويلة الأمد
+      علاقتنا بهم بينما نقوم بإثراء وتوسيع نطاق تأثيرنا ليشمل المجتمع بأكمله.`,
+    'twinningandfrienship.feature1.title': 'الأهداف',
+    'twinningandfrienship.feature2.title': 'أهداف',
+    'twinningandfrienship.feature3.title': 'أنشطة البرنامج',
+  },
 } as const;
 
 export const routes = {
